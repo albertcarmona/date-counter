17d73a7e2ce11552471fa5217b86b5249c2bcbcd
albertcarmona <albertcarmonahernandez@gmail.com>
2026-03-01 23:33:32 +0100

    Fix: Fixed not auto-updating time left

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-
 from datetime import datetime
 import streamlit as st
 import time
 
-now = datetime.now()
 final_date = datetime(2026, 3, 27, 0, 0, 0)
 
-time_left = final_date - now
-
-
 def main():
+    now = datetime.now()
+    time_left = final_date - now
+    
     st.title("Hans Zimmer Live Counter")
-    st.subheader(f"Barcelona\t\tConcert Date: {final_date}")
+    st.subheader(f"Barcelona Concert Date: {final_date}")
     st.subheader("Minutes left:")
-    time.sleep(10)
 
+    minutos_totales = int(time_left.total_seconds() // 60)
+    
     col1, col2, col3 = st.columns(3)
     with col2:
-        st.title(int(time_left.total_seconds() // 60))
+        st.title(minutos_totales)
 
+    time.sleep(10)
     st.rerun()
-
 
 if __name__ == "__main__":
     main()
