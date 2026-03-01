e973718d8ac092a2ed491b6061a49a8367c35049
albertcarmona <albertcarmonahernandez@gmail.com>
2026-03-02 00:29:21 +0100

    Changed app format

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import streamlit as st
 import pytz
 import time
 
-madrid_tz = pytz.timezone('Europe/London')
+madrid_tz = pytz.timezone('Europe/Madrid')
 final_date = madrid_tz.localize(datetime(2026, 3, 27, 0, 0, 0))
 
 def main():
-    now = datetime.now()
+    now = datetime.now(madrid_tz)
     time_left = final_date - now
     
-    st.title("Hans Zimmer Live Counter")
+    st.title("Hans Zimmer Live Counter⏳")
     st.subheader(f"Barcelona Concert Date: {final_date}")
     st.subheader("Minutes left:")
 
-    minutos_totales = int(time_left.total_seconds() // 60)
+    total_minutes = int(time_left.total_seconds() // 60)
     
     col1, col2, col3 = st.columns(3)
     with col2:
-        st.title(minutos_totales)
+        st.title(f"{total_minutes}")
 
     time.sleep(10)
     st.rerun()
